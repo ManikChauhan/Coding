41ab6f9e333f7efe5806fd037a7a71b4bc0d8055
ManikChauhan <84540602+ManikChauhan@users.noreply.github.com>
2021-06-30 23:48:07 +0530

    Create toffee.cpp

---
+++ toffee.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    vector<string> b;
+    int i,z=0,max=0,xtra=0,n;
+    cout<<"Enter the value";
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>z;
+        v.push_back(z);
+    }
+    cout<<"Enter extra toffee";
+    cin>>xtra;
+    max = *max_element(v.begin(),v.end());
+    for(i=0;i<n;i++)
+    {
+        if(v[i]+xtra>=max)
+        {
+            b.push_back("true");
+        }
+        else{
+            b.push_back("false");
+        }
+    }
+    for(i=0;i<b.size();i++)
+    {
+        cout<<b[i]<<" ";
+    }
+}
